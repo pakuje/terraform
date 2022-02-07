0b1d2ade684d1c6895f1d9b5e8cedb243732d869
pakuje <pakuje1@gmail.com>
2022-02-07 13:35:41 +0900

    second commit

--- resource_group.tf
+++ resource_group.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg03" {
   name     = "dev_pkj_rg01"
   location = var.location
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
