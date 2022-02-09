d35027fdb5a059dd5eee78e1769f4a035e42c6be
pakuje <pakuje1@gmail.com>
2022-02-09 10:21:42 +0900

    test2

--- resource_group.tf
+++ resource_group.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "rg03" {
-  name     = "dev_pkj_rg02"
+  name     = "dev_pkj_rg01"
   location = var.location
 }
-
+/*
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = var.location
